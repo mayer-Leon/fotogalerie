f2707bd802d4dc646eee8ddc58383fd7f7f2dbba
mayer-Leon <160941846+mayer-Leon@users.noreply.github.com>
2024-02-22 20:20:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function pic03434Function() {
+    pic03434Hover.style.display = "flex";
+}
+function pic03434FunctionOut() {
+    pic03434Hover.style.display = "none";
+}
+
+
+function pic03587Function() {
+    pic03587Hover.style.display = "flex";
+}
+function pic03587FunctionOut() {
+    pic03587Hover.style.display = "none";
+}
+
+
+function pic03281Function() {
+    pic03281Hover.style.display = "flex";
+}
+function pic03281FunctionOut() {
+    pic03281Hover.style.display = "none";
+}
+
+
+function pic03279Function() {
+    pic03279Hover.style.display = "flex";
+}
+function pic03279FunctionOut() {
+    pic03279Hover.style.display = "none";
+}
