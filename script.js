90fe9c6d960fb8114eb0eff8c1e70855f4dd8e30
mayer-Leon <160941846+mayer-Leon@users.noreply.github.com>
2024-03-01 11:38:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -624,6 +624,18 @@ function activateHover() {
     location.reload();
 }
 
+document.querySelectorAll('#toC a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault(); // Prevent default behavior of anchor tag
+      const targetId = this.getAttribute('href').substring(1); // Get the id of the target section
+      const targetElement = document.getElementById(targetId); // Find the target element
+      if (targetElement) {
+        // Scroll to the target element smoothly
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+
 changeContentForMobile();
 window.onload = adjustContent;
 window.onresize = adjustContent;
